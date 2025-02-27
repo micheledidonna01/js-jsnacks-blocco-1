e93a08fa0621a8bd97df9237b32823c5d8770de2
micheledidonna01 <micheledidonna337@gmail.com>
2025-02-27 13:03:01 +0100

    fix: solution fix

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,8 +2,8 @@
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
-let num1 = prompt('Inserisci il primo numero');
-let num2 = prompt('Inserisci il secondo numero');
+let num1 = Number(prompt('Inserisci il primo numero'));
+let num2 = Number(prompt('Inserisci il secondo numero'));
 
 if(num1 > num2){
     alert(`Il numero maggiore è ${num1}`);
--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,16 +1,14 @@
 // jsnack3
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
-let numbers;
-let numbersTenTimes;
+let number = 0;
 
 
 for (let i = 0; i < 10; i++) {
-    numbersTenTimes = prompt('inserisci un numero');
+    let numbersTenTimes = Number(prompt('inserisci un numero'));
     console.log(numbersTenTimes);
-    numbers += numbersTenTimes;
-    console.log(numbers);
-
+    number += numbersTenTimes;
+    console.log(number);
 }
-alert(`La somma è ${numbers}`);
-console.log(numbers);
+alert(`La somma è ${number}`);
+console.log(number);
